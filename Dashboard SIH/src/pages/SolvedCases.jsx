0c900d90ec87746c20b8715a13766c20329e5542
yashh13kumar <137873500+yashh13kumar@users.noreply.github.com>
2023-09-20 04:55:25 +0530

    Update SolvedCases.jsx

--- Dashboard SIH/src/pages/SolvedCases.jsx
+++ Dashboard SIH/src/pages/SolvedCases.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   GridComponent,
   ColumnsDirective,
@@ -10,28 +10,25 @@ import {
   Toolbar,
   Sort,
   Filter,
-} from "@syncfusion/ej2-react-grids";
-import Navbar from "../components/Navbar";
-import { customersData, customersGrid } from "../data/dummy";
-import { Header } from "../components";
+} from '@syncfusion/ej2-react-grids';
+import Navbar from '../components/Navbar';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const SolvedCases = () => {
   const selectionsettings = { persistSelection: true };
-  const toolbarOptions = ["Delete"];
+  const toolbarOptions = ['Delete'];
   const editing = { allowDeleting: true, allowEditing: true };
 
   const newCustomersData = customersData.filter((x) => {
-    return x.Status === "Released" || x.Status === "Prisoned";
+    return x.Status === 'Released' || x.Status === 'Prisoned';
   });
-
-//   console.log(customersData);
-//   console.log(newCustomersData);
 
   return (
     <div className="md:ml-10 md:mr-10 mt-2 md:pr-10 md:pl-10 bg-white rounded-3xl">
-         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              <Navbar />
-            </div>
+      <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+        <Navbar />
+      </div>
       <Header category="Page" title="Solved Cases" />
       <GridComponent
         dataSource={newCustomersData}
@@ -41,10 +38,8 @@ const SolvedCases = () => {
         selectionSettings={selectionsettings}
         toolbar={toolbarOptions}
         editSettings={editing}
-        // allowSorting
       >
         <ColumnsDirective>
-          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           {customersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
