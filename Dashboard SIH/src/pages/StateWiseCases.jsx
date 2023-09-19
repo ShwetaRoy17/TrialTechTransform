1c8f237a2fc22f2f14e64905a504d9f0fb617d70
yashh13kumar <137873500+yashh13kumar@users.noreply.github.com>
2023-09-20 04:55:44 +0530

    Update StateWiseCases.jsx

--- Dashboard SIH/src/pages/StateWiseCases.jsx
+++ Dashboard SIH/src/pages/StateWiseCases.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   GridComponent,
   ColumnsDirective,
@@ -10,43 +10,42 @@ import {
   Toolbar,
   Sort,
   Filter,
-} from "@syncfusion/ej2-react-grids";
-import Navbar from "../components/Navbar";
-import { customersData, customersGrid } from "../data/dummy";
-import { Header } from "../components";
+} from '@syncfusion/ej2-react-grids';
+import Navbar from '../components/Navbar';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const StateWiseCases = () => {
   const selectionsettings = { persistSelection: true };
-  const toolbarOptions = ["Delete"];
+  const toolbarOptions = ['Delete'];
   const editing = { allowDeleting: true, allowEditing: true };
-  const [selectState, setSelectState] = useState("Gujrat High Court");
+  const [selectState, setSelectState] = useState('Gujrat High Court');
 
-  const stateCahngeHandler = (event) => {
+  const stateChangeHandler = (event) => {
     setSelectState(event.target.value);
   };
 
   const uniqueStates = new Set();
   customersData.forEach((i) => uniqueStates.add(i.Location));
-  let uniqueStatesArray = [];
-  uniqueStates.forEach((states) => uniqueStatesArray.push(states));
-  console.log("ARRAY ", uniqueStatesArray);
-  console.log("STATE ", selectState);
+  const uniqueStatesArray = Array.from(uniqueStates);
+  console.log('ARRAY ', uniqueStatesArray);
+  console.log('STATE ', selectState);
 
   return (
     <div className="md:mr-10 md:ml-10 mt-2 md:pr-10 md:pl-10 bg-white rounded-3xl">
-         <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
-              <Navbar />
-            </div>
+      <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+        <Navbar />
+      </div>
       <Header category="Page" title="State Wise Cases" />
-      <select id="category" name="category" onChange={stateCahngeHandler}>
+      <select id="category" name="category" onChange={stateChangeHandler}>
         {uniqueStatesArray.map((states) => (
-          <option value={states}>{states}</option>
+          <option key={states} value={states}>
+            {states}
+          </option>
         ))}
       </select>
       <GridComponent
-        dataSource={customersData.filter((data) => {
-          return data.Location === selectState.toString();
-        })}
+        dataSource={customersData.filter((data) => data.Location === selectState)}
         enableHover={false}
         allowPaging
         pageSettings={{ pageCount: 5 }}
